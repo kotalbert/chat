07f91c4fd5c8b6c3bcec8f166db65cf015400565
Paweł Daniluk <pawel@daniluk.waw.pl>
2021-06-01 15:34:41 +0200

    RED: Minimal functional test for chat Client

---
+++ test/functional_tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestChatFunctional(unittest.TestCase):
+    def test_message_exchange(self):
+        """
+        Chat clients should be able to exchange messages between each other.
+        """
+        albert = ChatClient("Albert")
+        arwena = ChatClient("Arwena")
+
+        albert.send_message("Hello, give food.")
+        messages = arwena.fetch_messages()
+
+        self.assertListEqual(["Hello, give food"], messages)

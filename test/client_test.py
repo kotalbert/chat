91d6c250b9c67eb00b3df2ff465de85cf76ca319
Paweł Daniluk <pawel@daniluk.waw.pl>
2021-06-01 17:10:13 +0200

    RED: Client should be able to send message

--- test/client_test.py
+++ test/client_test.py
@@ -11,3 +11,11 @@ class TestChatClient(unittest.TestCase):
         """
         client = ChatClient('Albert')
         self.assertEqual(client.nickname, 'Albert')
+
+    def test_send_message(self):
+        """
+        Client should sent message in appropriate format.
+        """
+        client = ChatClient('Albert')
+        sent_message = client.send_message('Give food')
+        self.assertEqual(sent_message, 'Albert:Give food')
